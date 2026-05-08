cbac2e4079b37b99e235ef3ad81a88b6864a52af
Marwan280 <marw8660@gmail.com>
2026-05-08 21:54:59 +0300

    updates

--- src/routes.js
+++ src/routes.js
@@ -6,9 +6,9 @@ router.use('/admin', require('./modules/admin/admin.routes'));
 router.use('/doctor', require('./modules/doctor/doctor.routes'));
 router.use('/patient', require('./modules/patient/patient.routes'));
 router.use('/receptionist', require('./modules/receptionist/receptionist.routes'));
-router.use('/appointments', require('./modules/appointment/appointment.routes'));
-router.use('/results', require('./modules/resultFile/resultFile.routes'));
-router.use('/profiles', require('./modules/patientProfile/patientProfile.routes'));
-router.use('/notifications', require('./modules/notification/notification.routes'));
+//router.use('/appointments', require('./modules/appointment/appointment.routes'));
+//router.use('/results', require('./modules/resultFile/resultFile.routes'));
+//router.use('/profiles', require('./modules/patientProfile/patientProfile.routes'));
+//router.use('/notifications', require('./modules/notification/notification.routes'));
 
 module.exports = router;
